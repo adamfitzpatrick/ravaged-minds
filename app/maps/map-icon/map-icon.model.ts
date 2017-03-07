9a16ccbaf09b5bd8f7f686be1a72d71871294ceb
Adam Fitzpatrick <adam.fitzpatrick@hobsons.com>
2017-03-06 21:21:13 -0800

    Added content and minor redesigns

--- app/maps/map-icon/map-icon.model.ts
+++ app/maps/map-icon/map-icon.model.ts
@@ -10,11 +10,60 @@ export interface MapIcon {
 export const MAP_ICONS: MapIcon[] = [{
     name: "site",
     subGroup: [{
-        name: "shop",
+        name: "public",
         subGroup: [{
             label: true,
             name: "tavern",
             icon: "group_work"
+        }, {
+            label: true,
+            name: "fairground",
+            icon: "stars"
+        }, {
+            label: true,
+            name: "market",
+            icon: "monetization_on"
+        }]
+    }, {
+        name: "private",
+        subGroup: [{
+            label: true,
+            name: "home",
+            icon: "home"
+        }, {
+            label: true,
+            name: "business",
+            icon: "business"
+        }]
+    }, {
+        name: "holy",
+        subGroup: [{
+            label: true,
+            name: "shrine",
+            icon: "change_history"
+        }, {
+            label: true,
+            name: "church",
+            icon: "account_balance"
+        }]
+    }, {
+        name: "government",
+        subGroup: [{
+            label: true,
+            name: "castle",
+            icon: "flag"
+        }, {
+            label: true,
+            name: "barracks",
+            icon: "security"
+        }, {
+            label: true,
+            name: "gatehouse",
+            icon: "block"
+        }, {
+            label: true,
+            name: "secure area",
+            icon: "games"
         }]
     }]
 }, {
